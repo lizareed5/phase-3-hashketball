7b5bdc70e2d9c32cc8eef4884f611168725f8239
Liza McLain <lizamclain@gmail.com>
2023-01-30 11:20:48 -0600

    commit tree c4194c5dee6fcb402882f5f1a1e8a8d91dbe54df

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,4 @@
+require "pry"
 # Write your code below game_hash
 def game_hash
   {
@@ -126,4 +127,63 @@ def game_hash
   }
 end
 
+# Helper methods
+def all_players
+  game_hash[:home][:players] + game_hash[:away][:players]
+
+end
+
+def get_player_by_name(player_name)
+  all_players.find do |player|
+    player[:player_name] == player_name
+  end
+end
+
+def get_team_by_name(team_name)
+  team_info = game_hash.find do |key, value|
+    value[:team_name] == team_name
+  end
+  team_info[1]
+end
+
 # Write code here
+def num_points_scored player_name
+  get_player_by_name(player_name)[:points]
+end
+
+def shoe_size player_name
+  get_player_by_name(player_name)[:shoe]
+end
+
+def team_colors team_name
+  get_team_by_name(team_name)[:colors]
+end
+
+def team_names
+  game_hash.map do |team_location, team_info|
+    team_info[:team_name]
+  end
+end
+
+def player_numbers team_name
+  proper_team = game_hash.find do |key, value|
+    value[:team_name] == team_name
+  end
+  proper_team[1][:players].map do |player|
+    player[:number]
+  end
+end
+
+def player_stats player_name
+  get_player_by_name(player_name)
+
+end
+
+def big_shoe_rebounds
+  largest_shoe_player = all_players.max do |a, b|
+   a[:shoe] <=> b[:shoe]
+  end
+  largest_shoe_player[:rebounds]
+end
+
+binding.pry
